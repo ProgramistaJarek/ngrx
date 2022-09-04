2f329ae44a992e0741cd98347f11d206454854d2
ProgramistaJarek <dojarka0@gmail.com>
2022-09-04 14:13:35 +0200

    test

--- src/app/features/cart/cart.component.spec.ts
+++ src/app/features/cart/cart.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { CartComponent } from './cart.component';
 
@@ -6,15 +7,18 @@ describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
 
+  let store: MockStore;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
-    })
-    .compileComponents();
+      declarations: [CartComponent],
+      providers: [provideMockStore()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store = TestBed.inject(MockStore);
   });
 
   it('should create', () => {
